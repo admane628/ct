a2e54ba0cc43886c6449ffeb2c411323fa3dd92e
admane628 <admane628@gmail.com>
2024-12-19 12:14:56 +0100

    Add files via upload

--- uo/views.py
+++ uo/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Formation, UE
 
 # Create your views here.
 def about(request):
     return render(request, "about.html")
+
+def formation(request, id):
+    fm = get_object_or_404(Formation, id=id)
+    ues = UE.objects.filter(formations=fm)
+    return render(request, "formation.html", {"formation": fm, "ues":ues})
