84562d273fafa524cee8e7882d46b2b1790db647
admane628 <admane628@gmail.com>
2024-12-19 11:46:43 +0100

    Add files via upload

--- uo/admin.py
+++ uo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Formation, UE
 
 # Register your models here.
+
+admin.site.register(Formation)
+admin.site.register(UE)
--- uo/models.py
+++ uo/models.py
@@ -4,13 +4,6 @@ from django.dispatch import receiver
 from django.contrib.auth.models import User
 
 # Create your models here.
-@receiver(post_migrate)
-def create_enseignants(sender, **kwargs):
-    User.objects.get_or_create(username='e1', email='e1@e.com', password='p')
-    User.objects.get_or_create(username='e2', email='e2@e.com', password='p')
-    User.objects.get_or_create(username='e3', email='e3@e.com', password='p')
-    User.objects.get_or_create(username='e4', email='e4@e.com', password='p')
-
 class Formation(models.Model):
     intitule = models.CharField(max_length =100)
     description = models.TextField()
@@ -18,3 +11,31 @@ class Formation(models.Model):
 
     def __str__(self):
         return self.intitule
+
+class UE(models.Model):
+    titre = models.CharField(max_length =100, null=False)
+    description = models.TextField(null=False)
+    CM = models.PositiveIntegerField(null=False)
+    TD = models.PositiveIntegerField(null=False)
+    TP = models.PositiveIntegerField(null=False)
+    credits = models.PositiveIntegerField(null=False)
+    formations = models.ManyToManyField(Formation)
+    responsables = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.titre
+
+@receiver(post_migrate)
+def create_enseignants(sender, **kwargs):
+    User.objects.get_or_create(username='e1', email='e1@e.com', password='p')
+    User.objects.get_or_create(username='e2', email='e2@e.com', password='p')
+    User.objects.get_or_create(username='e3', email='e3@e.com', password='p')
+    User.objects.get_or_create(username='e4', email='e4@e.com', password='p')
+
+
+@receiver(post_migrate)
+def create_formations(sender, **kwargs):
+    Formation.objects.get_or_create(intitule='Licence Informatique — Parcours Ingénierie informatique')
+    Formation.objects.get_or_create(intitule='Licence Informatique — Parcous MIAGE')
+    Formation.objects.get_or_create(intitule='Master Informatique')
+    Formation.objects.get_or_create(intitule='Master MIAGE')
