7000e2fc5abc15f122eb11a71b536a7401c9635f
admane628 <admane628@gmail.com>
2024-12-19 15:27:10 +0100

    Add files via upload

--- uo/views.py
+++ uo/views.py
@@ -3,23 +3,50 @@ from .models import Formation, UE
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
-# Create your views here.
+#### Q19 ####
+
+# Il faut ajouter @login_required à toutes les pages. q19
+
+## Il faut ajouter ça aux fonction d'ajouts, modification, suppression de UE -> pour gerer les autorisations:
+
+# responsables =  []
+# fms = Formation.objects.all()
+# for fm in fms:
+	# if not fm.responsable in responsables:
+		# responsables.append(fm.responsable)
+
+# if not request.user in responsables and not request.user.is_superuser:
+	# return HttpResponse('Unauthorized', status=401)
+
+## Il faut ajouter ça après le code précédent juste dans la fonction de modification:
+
+# ue = get_object_or_404(UE, id=id)
+# if not request.user in ue.responsables.all():
+	# return HttpResponse('Unauthorized', status=401)
+
+#### Q19 ####
+
+@login_required
 def about(request):
     return render(request, "about.html")
 
+@login_required
 def formation(request, id):
     fm = get_object_or_404(Formation, id=id)
     ues = UE.objects.filter(formations=fm)
     return render(request, "formation.html", {"formation": fm, "ues":ues})
 
+@login_required
 def ue_view(request, id):
     u = get_object_or_404(UE, id=id)
     return render(request, "ue_view.html", {"ue":u})
 
+@login_required
 def formations(request):
     fm = Formation.objects.all()
     return render(request, "formations.html", {"fm":fm})
 
+@login_required
 def ues(request):
     ues = UE.objects.all()
     return render(request, "ues.html", {"ues":ues})
@@ -40,7 +67,7 @@ def exclusive(request): #q20)
 
     data = []
     for formation in fms:
-        ue_attaches = UE.objects.filter(formations=fm)
+        ue_attaches = UE.objects.filter(formations=formation)
         nombre_ue_attache = ue_attaches.count()
         responsables_noms =  []
 
